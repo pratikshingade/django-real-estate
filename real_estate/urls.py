c732043d8817bdf9be43d2a98b7b92413cbb9dde
pratikshingade <pratik.shingade04@gmail.com>
2025-01-23 18:41:12 +0530

    chore: setup docker with compose

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -13,8 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -27,7 +25,7 @@ urlpatterns = [
                   path('api/v1/properties/', include('apps.properties.urls')),
                   path('api/v1/enquiries/', include('apps.enquiries.urls')),
                   path('api/v1/ratings/', include('apps.ratings.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]  # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = 'Real Estate Admin'
 admin.site.site_title = 'Real Estate Admin Portal'
